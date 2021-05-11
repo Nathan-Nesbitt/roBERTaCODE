645c7ce2b03f61a4cf2093c9056564af4e534625
Nathan <Nathan-Nesbitt@users.noreply.github.com>
2021-05-11 13:10:51 -0700

    Added full setup script for pre-training

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 # Sets up the environment for development
 sh scripts/venv.sh
 # Loads all of the data from CodeSearchNet
-python scripts/load_data.py
+python3 scripts/load_data.py
 # Cleans the data from CodeSearchNet
-python scripts/clean_data.py
+python3 scripts/clean_data.py --languages all --sizes large -c
+python3 scripts/clean_data.py --languages java --sizes large
+python3 scripts/clean_data.py --languages python --sizes large
+# Creates the tokenizers for each of the datasets
+python3 scripts/generate_tokenizer.py --languages all --sizes large -c
+python3 scripts/generate_tokenizer.py --languages python --sizes large
+python3 scripts/generate_tokenizer.py --languages java --sizes large
