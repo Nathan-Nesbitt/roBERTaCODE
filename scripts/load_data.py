94d019789fd135a2bf5803d28e7ae912a2729d7b
Nathan <Nathan-Nesbitt@users.noreply.github.com>
2021-05-11 18:50:19 -0700

    Fixed Load Data to Download and display stats

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -7,91 +7,93 @@
 """
 
 import argparse
-import requests
-import zipfile
 from zipfile import ZipFile
-from io import BytesIO
 import os
 import sys
-import gzip
-from pathlib import Path
-import json
+from urllib.request import urlopen
+from multiprocessing import Process
+from sh import gunzip
+import glob
+from tqdm import tqdm
+from multiprocessing import Pool
 
-parser = argparse.ArgumentParser(
-    prog="load_data.py", description="Downloads dataset from CodeSearchNet"
-)
-
-parser.add_argument(
-    "language",
-    metavar="lang",
-    type=str,
-    nargs="?",
-    help="Indicates Language Type for Download. \
-        Can be python, php, java, go, javascript or ruby",
-)
-
-
-def get_data(language):
-    # The file locations as defined in the readme
-    filename = (
-        "https://s3.amazonaws.com/code-search-net/CodeSearchNet/v2/" + language + ".zip"
+def main():
+    
+    parser = argparse.ArgumentParser(
+        prog="load_data.py", description="Downloads dataset from CodeSearchNet"
     )
+    
+    parser.add_argument(
+        "--languages",
+        "-l",
+        metavar="lang",
+        type=str,
+        nargs="?",
+        help="Indicates Language Type for Download. \
+            Can be python, php, java, go, javascript or ruby. None downloads all.",
+    )
+    # Parses the arguments passed to the script
+    args = parser.parse_args()
 
+    a = []
+    a.append(args.languages)
+    args.languages = a
+
+    # Creates the data directory
     try:
-        os.mkdir("./data/" + language)
+        os.mkdir(os.path.dirname("./data/"))
     except FileExistsError:
         pass
 
+    languages = ["python", "php", "java", "go", "javascript", "ruby"]
+
+    # Handles all conditions for the parameters
+    if args.languages == [None]:
+        args.languages = languages
+        
+    elif not set(languages).issubset(set(args.languages)):
+        sys.stderr.write(
+            "Please specify a real language in languages: {}\n".format(languages)
+        )
+    with Pool(6) as p:
+        input = []
+        for i in range(len(args.languages)):
+            input.append((args.languages[i], i))
+        p.starmap(download_file, input)
+
+def download_file(language, position):
+    location = "https://s3.amazonaws.com/code-search-net/CodeSearchNet/v2/{}.zip".format(language)
     # Get the file from the server
-    requested_file = requests.get(filename, stream=True)
-    # Unzip it
-    zipped_files = ZipFile(BytesIO(requested_file.content), "r")
+    requested_file = urlopen(location)
+    total_length = requested_file.headers.get('content-length')
+    if total_length:
+        total_length = int(total_length)
+        blocksize = max(4096, total_length//100)
+    else:
+        blocksize = 1000000 
+    with open("/tmp/{}.zip".format(language), "wb") as tempzip:
+        with tqdm(total=total_length, position=position) as tq:
+            tq.set_description("Downloading {}".format(language))
+            while True:
+                data = requested_file.read(blocksize)
+                if not data:
+                    break
+                tempzip.write(data)
+                tq.update(blocksize)
+    with ZipFile("/tmp/{}.zip".format(language)) as zf:
+        zf.extractall(path="./data/")
+    # Finally delete the temp file 
+    os.remove("/tmp/{}.zip".format(language))
+    # Get all of the zipped files and extract them
+    files = []
+    for file in glob.glob("data/{}/**/*.gz".format(language), recursive=True):
+        files.append(file)
+    with tqdm(total=len(files), position=position) as tq:
+        tq.set_description("Unzipping {}".format(language))
+        for i in files:
+            gunzip(i)
+            tq.update(1)
 
-    for zipped_file in zipped_files.filelist:
-        # If the file is a folder
-        if zipped_file.is_dir():
-            try:
-                os.mkdir("./data/" + language)
-            except FileExistsError:
-                pass
-        # Else we are dealing with a file
-        else:
-            # Creates path
-            Path("./data/" + zipped_file.filename).parent.absolute().mkdir(
-                parents=True, exist_ok=True
-            )
-            # Deals with gzipped file extentions
-            file_path = "./data/" + zipped_file.filename
-            if file_path[-2:] == "gz":
-                file_path = file_path[:-3]
-            # Opens and writes the file
-            with open(file_path, "wb+") as write_file:
-                f = zipped_files.read(zipped_file.filename)
-                # Because we have double zipped files in here, we decompress
-                if zipped_file.filename[-2:] == "gz":
-                    write_file.write(gzip.decompress(f))
-                else:
-                    write_file.write(f)
 
-
-# Parses the arguments passed to the script
-args = parser.parse_args()
-
-# Creates the data directory
-try:
-    os.mkdir(os.path.dirname("./data/"))
-except FileExistsError:
-    pass
-
-languages = ["python", "php", "java", "go", "javascript", "ruby"]
-
-# Handles all conditions for the parameters
-if not args.language:
-    for i in languages:
-        get_data(i)
-elif args.language not in languages:
-    sys.stderr.write(
-        "Please specify a real language in languages: " + str(languages) + "\n"
-    )
-else:
-    get_data(args.language)
+if __name__ == "__main__":
+    main()
