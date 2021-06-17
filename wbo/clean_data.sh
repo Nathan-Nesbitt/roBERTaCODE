383b3dd677782c015dfb0c6f9cfc7928961ffb00
Nathan <Nathan-Nesbitt@users.noreply.github.com>
2021-06-17 14:02:32 -0700

    Fixed it so you can clean data separately from WBO

---
+++ wbo/clean_data.sh
@@ -0,0 +1,5 @@
+. ../venv/bin/activate
+pip install gdown
+gdown https://drive.google.com/uc?id=1f4y6oLg1745KvHMILYfLKHkQLvK3BWxh
+unzip ETH\ Py150\ Open.zip
+mv ETH\ Py150\ Open data
--- wbo/fine_tune.sh
+++ wbo/fine_tune.sh
@@ -1,5 +1,5 @@
+# Just incase you forgot to use the venv
 . ../venv/bin/activate
-pip install gdown
-gdown https://drive.google.com/uc?id=1f4y6oLg1745KvHMILYfLKHkQLvK3BWxh
-unzip ETH\ Py150\ Open.zip
-mv ETH\ Py150\ Open data
+
+# Runs through the scripts
+python3 wbo_train.py
