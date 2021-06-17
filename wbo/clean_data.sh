1f9d27cfceed04c658941e62c2ed80de9601953c
Nathan <Nathan-Nesbitt@users.noreply.github.com>
2021-06-17 14:17:06 -0700

    Fixed missing script to clean data

--- wbo/clean_data.sh
+++ wbo/clean_data.sh
@@ -3,3 +3,5 @@ pip install gdown
 gdown https://drive.google.com/uc?id=1f4y6oLg1745KvHMILYfLKHkQLvK3BWxh
 unzip ETH\ Py150\ Open.zip
 mv ETH\ Py150\ Open data
+rm ETH\ Py150\ Open.zip
+python clean_data.py
