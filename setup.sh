053f107f90d7331ed121141d642ad1cc1099e605
Nathan Nesbitt <Nathan-Nesbitt@users.noreply.github.com>
2021-06-28 15:20:51 -0700

    Removed the old setup style
    Instead of running the code from the setup script in the root, we have an individual file for getting the PTM data

--- setup.sh
+++ setup.sh
@@ -2,6 +2,3 @@
 python3 -m venv venv
 . ./venv/bin/activate
 pip install -r requirements.txt
-
-# Generates all of the data for a combined, python, and java datasets
-python3 pre_train/data.py --languages all python java --sizes large
